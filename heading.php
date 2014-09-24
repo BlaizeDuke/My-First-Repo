b72ff031c35b938307db8e4b6b9186e851f572f0
BlaizeDuke <Blaize710@gmail.com>
2014-09-24 03:05:41 -0500

    Adding Bootstrap

---
+++ heading.php
@@ -0,0 +1,11 @@
+<html>
+
+	<head>
+		<title>First Repo</title>
+		<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<script src="js/bootstrap.js"></script>
+		<script src="js/bootstrap.js"></script>
+	</head>
+	
+	<body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?PHP 
+	include_once('heading.php');
+	
+	//Content
+	echo '<h1>Testing</h1>';
+	
+	//Footing
+	include_once('footing.php');
+?>
